7dcadb4c6213a56fae8d37faa4d80c49b31d7b0e
hoejun <hoijunkim1203@gmail.com>
2025-07-15 01:59:46 +0900

    comment add

--- src/main/java/com/khj/dev/common/security/SecurityConfig.java
+++ src/main/java/com/khj/dev/common/security/SecurityConfig.java
@@ -8,6 +8,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+	/*
+	 * 스프링 부트 프로젝트에 Spring Security 의존성이 포함되어 있거나, spring-boot-starter-security가
+	 * 자동으로 설정되면 다음과 같은 보안 설정이 기본으로 적용됩니다:
+	 * 모든 엔드포인트는 인증 없이는 접근 불가
+	 * 인증되지 않은 요청은 /login 페이지로 리다이렉트
+	 * 결과적으로 HTML 로그인 페이지가 반환됨
+	 */
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
